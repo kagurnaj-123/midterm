e9658a1db7bd20bf35f02bec2004d256bbb17928
kagurnaj-123 <158625763+kagurnaj-123@users.noreply.github.com>
2024-02-23 10:19:25 -0500

    Update Weekdays.java

--- src/days/Weekdays.java
+++ src/days/Weekdays.java
@@ -1,19 +1,52 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package days;
 
 /**
- *
+ * Represents weekdays and provides a method to print the name of the day based on the code.
+ * 
  * @author sivagamasrinivasan
  */
-public class Weekdays 
-{
-  public void nameOfDay(String code)
-     {
-    switch(code.toUpperCase())
-      {
+public class Weekdays {
+  
+  /**
+   * Prints the name of the day based on the provided Day enum.
+   * 
+   * @param day The Day enum representing the day.
+   */
+  public void nameOfDay(Day day) {
+    switch (day) {
+      case ONE:
+        System.out.println("Monday");
+        break;
+      case TWO:
+        System.out.println("Tuesday");
+        break;
+      case THREE:
+        System.out.println("Wednesday");
+        break;
+      case FOUR:
+        System.out.println("Thursday");
+        break;
+      case FIVE:
+        System.out.println("Friday");
+        break;
+      case SIX:
+        System.out.println("Saturday");
+        break;
+      case SEVEN:
+        System.out.println("Sunday");
+        break;
+      default:
+        System.out.println("Invalid day");
+    }
+  }
+
+  /**
+   * Prints the name of the day based on the provided code.
+   * 
+   * @param code The code representing the day.
+   */
+  public void nameOfDay(String code) {
+    switch (code.toUpperCase()) {
       case "ONE":
         System.out.println("Monday");
         break;
@@ -30,14 +63,4 @@ public class Weekdays
         System.out.println("Friday");
         break;
       case "SIX":
-        System.out.println("Saturday");
-        break;
-      case "SEVEN":
-        System.out.println("Sunday");
-        break;
-    }
-  
-}
-}
-
-
+        System.out.println("
